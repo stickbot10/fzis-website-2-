6436920b4ec005dd53322e5bc63967af9562fcc0
stickbot10 <stickbot212@hotmail.com>
2025-12-03 09:44:00 +1000

    Fix API routes: update hello.js to CommonJS and simplify vercel.json

---
+++ api/hello.js
@@ -0,0 +1,12 @@
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  
+  res.status(200).json({ message: 'hello from vercel' });
+};
